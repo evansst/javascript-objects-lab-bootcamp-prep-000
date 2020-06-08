415271dbaf27463508699f56c15444920a779c22
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 06:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function updateObjectWithKeyAndValue(obj1,key,value) {
   return Object.assign({}, obj, { [key]: value })
 }
-function 
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value)
+{
   var obj2 = new Object(obj1)
   obj2[key] = value
   return obj2;
